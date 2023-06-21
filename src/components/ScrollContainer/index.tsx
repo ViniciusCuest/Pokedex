50f8c6bf449a64b81fad4e8c4dd5eb837711e38a
Vinicius da Silva Costa <vinicius.c@compasso.com.br>
2023-06-21 18:07:21 -0300

    fixed text position

--- src/components/ScrollContainer/index.tsx
+++ src/components/ScrollContainer/index.tsx
@@ -10,8 +10,9 @@ export function ScrollContainer({ data }: DataProps) {
    useEffect(() => { console.log(data) }, []);
    return (
       <main>
+         <h1 className='text-3xl font-bold font-sans text-black_900 mb-4 sm:mb-8 sm:text-6xl sm:absolute sm:ml-10 sm:mt-4'>Favoritos</h1>
          <section
-            className='relative pt-10 px-5 sm:pt-20 sm:mt-20 sm:pl-14 sm:pr-[34rem] sm:mr-9 sm:overflow-x-scroll sm:overflow-y-hidden'>
+            className='relative pt-10 px-5 sm:pt-32 sm:mt-14 sm:pl-14 sm:pr-[34rem] sm:mr-9 sm:overflow-x-scroll sm:overflow-y-hidden'>
             <div className='grid w-auto gap-4 gap-x-4 grid-cols-2 sm:flex sm:flex-row sm:gap-x-11 sm:min-w-fit sm:p-5 sm:ml-[-.9rem]'>
                {
 
