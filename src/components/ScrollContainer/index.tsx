25686a850810c036f9f092553593eb563afd9d71
Vinicius da Silva Costa <vinicius.c@compasso.com.br>
2023-06-23 16:29:33 -0300

    scroll sections fixed

--- src/components/ScrollContainer/index.tsx
+++ src/components/ScrollContainer/index.tsx
@@ -17,27 +17,40 @@ export function ScrollContainer({ data }: DataProps) {
   const [currentCardWidth, setCurrentCardWidth] = useState<number>(0);
 
   const handleChangeAppearingList = (next: boolean = false) => {
+
     const toIndex: number = next ? currentIndex + 1 : currentIndex - 1;
+
+    console.log('CÁLCULO INDEX > 1: ', (currentCardWidth * 3 + (cardsGap * 3)) * toIndex + 17);
+    //console.log('INDEX == 1', currentCardWidth * toIndex * 3 + (cardsGap * 3 + 18))
+
+
     scrollComponentRef.current?.scrollTo({
       behavior: 'smooth',
-      left: (currentCardWidth + cardsGap) * (toIndex * 3)
+      left: toIndex === 1 ? currentCardWidth * 3 + (cardsGap * 3 + 18) : (currentCardWidth * 3 + (cardsGap * 3)) * toIndex + 17
     });
+
     setCurrentIndex(toIndex);
+
   }
   //.filter((i, index) => { return index >= data.length - 10 }).reverse()
   return (
     <main>
-      <h1 className='text-3xl font-bold font-sans text-black_900 mb-4 sm:mb-8 sm:text-6xl sm:absolute sm:ml-10 sm:mt-4'>Favoritos</h1>
+      <h1 className='text-3xl font-bold font-sans text-black_900 mb-4 sm:mb-8 sm:text-6xl sm:absolute sm:ml-10 sm:mt-4'>
+        Favoritos
+      </h1>
       <section
         ref={scrollComponentRef}
-        onLoad={(e) => {
-          setMaxScrollableLength(e.currentTarget.scrollWidth)
+        onClick={(e) => {
+          setCurrentIndex(0);
+          //console.log('=== valor do SCROLL: ', e.currentTarget?.scrollLeft)
         }}
-        className='relative pt-10 px-5 sm:pt-32 sm:mt-14 sm:pl-14 sm:pr-[34rem] sm:mr-9 sm:overflow-x-scroll sm:overflow-y-hidden'>
+        onLoad={(e) => {
+          //setMaxScrollableLength(e.currentTarget.scrollWidth + 592 + 56)
+        }}
+        className='relative pt-10 px-5 sm:pt-32 sm:mt-14 sm:pl-14 sm:pr-[37rem] sm:mr-9 sm:overflow-x-scroll sm:overflow-y-hidden'>
         <div
           onLoad={(e) => {
             const { clientWidth } = e.currentTarget;
-            console.log('Width: ', clientWidth)
           }}
           className='grid w-auto gap-4 gap-x-4 grid-cols-2 sm:flex sm:flex-row sm:gap-x-11 sm:min-w-fit sm:p-5 sm:ml-[-.9rem]'>
           {
